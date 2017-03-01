598836cd85a67653b4fe4167c08370df7bf489d2
123saarthi <123saarthi@gmail.com>
2017-03-01 09:21:24 +0530

    Pattern Program

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pattern;
+
+class pattern
+{ public static void main(String args[])
+ { System.out.println("Screen Display");
+   for(int i=1;i<=9;i++)
+   { for(int j=1;j<=i;j++)
+       {System.out.print("");
+       System.out.print(+i);
+       }
+   System.out.print("\n");
+   }
+ }
+}
+        
