f984b325e4d1c39cc7728804678f4fafd8cbb73b
123saarthi <123saarthi@gmail.com>
2017-03-01 09:35:18 +0530

    Relational Operators

---
+++ RealtionalOperators.java
@@ -0,0 +1,14 @@
+class RealtionalOperators
+{ public static void main(String args[])
+  { float a=15.0f,b=20.75f,c=15.0f;
+    System.out.println("a="+a);
+    System.out.println("b="+b);
+    System.out.println("c="+c);
+    System.out.println("a<b is"+(a<b));
+    System.out.println("a>b is"+(a>b));
+    System.out.println("a==c is"+(a==c));
+    System.out.println("a<=c is"+(a<=c));
+    System.out.println("b!=c is"+(b!=c));
+    System.out.println("b==a+c is"+(b==a+c));
+  }
+}
